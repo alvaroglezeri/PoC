c5fd37a0e28cc71fa12c415cb714ef08516a1369
alvaroglezeri <alvaroglezeri@protonmail.com>
2024-09-28 15:54:16 +0200

    Created BPM Predictor

--- src/core/analysis/step1.py
+++ src/core/analysis/step1.py
@@ -1,6 +1,10 @@
 import librosa
 import numpy as np
 from typing import BinaryIO
+from deeprhythm import DeepRhythmPredictor
+
+import warnings
+
 
 class KeyAnalysis():
     
@@ -8,7 +12,11 @@ class KeyAnalysis():
     CHROMA_TO_KEY = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     
     @staticmethod
-    def getKey(file: BinaryIO) -> str:        
+    def getKey(file: BinaryIO) -> str:
+        """
+        DOCUMENT
+        https://medium.com/@oluyaled/detecting-musical-key-from-audio-using-chroma-feature-in-python-72850c0ae4b1
+        """   
         # Load the audio file from the beginning: https://github.com/bastibe/python-soundfile/issues/333
         # PROBLEM: Take note of this
         file.seek(0)  
@@ -29,9 +37,43 @@ class KeyAnalysis():
     
 class BeatAnalysis():
     @staticmethod
-    def getBeat(file: BinaryIO) -> str:        
-        return '(No BPM)'
+    def getBeat(file: BinaryIO) -> tuple[float, float, list[float]]:
+        file.seek(0)
+        model = DeepRhythmPredictor(quiet=True)
+
+        #Silencing these warnings, as there is nothing I can do about them
+        #with warnings.catch_warnings():
+        #    warnings.simplefilter("ignore")
+
+        #As the audio is loaded with Librosa, it supports loading an BinaryIO file
+        bpm, confidence = model.predict(file, include_confidence=True)
+
+        file.seek(0)
+        y, sr = librosa.load(file)
+        _, beats = librosa.beat.beat_track(y=y, bpm = bpm)
+        beats = librosa.frames_to_time(beats, sr=sr)
+
+        return bpm, confidence, beats
+
+        
     
+    @staticmethod
+    def getBeat_outdated(file: BinaryIO) -> float:
+        # Load the audio file        
+        file.seek(0)
+        y, sr = librosa.load(file)
+
+        bpm, beats = librosa.beat.beat_track(y=y ,sr=sr)
+
+        pulse = librosa.beat.plp(y=y, sr=sr)
+        beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
+
+        data: dict = {
+            'beat_track': (bpm, beats), 
+            'plp': (beats_plp)
+        }
+
+        return data 
     
     
     
